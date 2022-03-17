d0d087cb596b5cde1e8c8966ea7185f6231064ab
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2022-03-18 01:08:46 +0330

    Create Seed Data

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,48 @@
+namespace PlatformService.Data;
+public static class PrepDb
+{
+    public static void PrepPopulation(this IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            SeedData(context: serviceScope.ServiceProvider.GetRequiredService<AppDbContext>());
+        }
+
+    }
+    private static void SeedData(AppDbContext context)
+    {
+        if (context is null)
+        {
+            return;
+        }
+        if (context.Platforms?.Any() is false)
+        {
+            Console.WriteLine("--> Seeding Data...");
+            context.Platforms.AddRange(
+                new Platform
+                {
+                    Name = "Dot Net",
+                    Publisher = "microsoft",
+                    Cost = "Free"
+                },
+                new Platform
+                {
+                    Name = "SQL Server Express",
+                    Publisher = "Microsoft",
+                    Cost = "Free"
+                },
+                new Platform
+                {
+                    Name = "Kubernetes",
+                    Publisher = "Cloud Nitave Computing Foundation",
+                    Cost = "Free"
+                }
+            );
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("--> We already have data");
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,23 +1,19 @@
-using System.Collections.Immutable;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<AppDbContext>();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.PrepPopulation();
 app.Run();
